a1aa92bcbd8523533f97d59bded56ff3242c1526
burlapjack <burlapjack@yahoo.com>
2021-07-19 18:27:30 -0500

    Minor changes to entity_add_component functions

--- include/entity.h
+++ include/entity.h
@@ -5,8 +5,8 @@
 #ifndef ENTITY_H
 #define ENTITY_H
 
-void entity_add_component_position(unsigned int id, int x,int y);
-void entity_add_component_draw(unsigned int id, int xoff, int yoff,int col,char sym);
-void entity_add_component_stats(unsigned int id, int hp, int str, int per, int agi);
+void entity_add_component_position(unsigned int id, ComponentPosition *c, int x,int y);
+void entity_add_component_draw(unsigned int id, ComponentDraw *d, int xoff, int yoff,int col,char sym);
+void entity_add_component_stats(unsigned int id, ComponentStats *s, int hp, int str, int per, int agi);
 
 #endif /*ENTITY_H*/
--- src/entity.c
+++ src/entity.c
@@ -3,14 +3,14 @@
 #include "../include/component.h"
 #include "../include/entity.h"
 
-void entity_add_component_position(unsigned int id, int x, int y){
+void entity_add_component_position(unsigned int id, ComponentPosition *c, int x, int y){
+			
+}
+
+void entity_add_component_draw(unsigned int id, ComponentDraw *d, int xoff, int yoff, int col, char sym){
 
 }
 
-void entity_add_component_draw(unsigned int id, int xoff, int yoff, int col, char sym){
-
-}
-
-void entity_add_component_stats(unsigned int id, int hp, int str, int per, int agi){
+void entity_add_component_stats(unsigned int id, ComponentStats *s, int hp, int str, int per, int agi){
 
 }

524b38ebf95b2e38d6b16947afb8d5854368ac99
burlapjack <burlapjack@yahoo.com>
2021-07-21 23:41:35 -0500

    Rewrote the add-component functions.

--- include/entity.h
+++ include/entity.h
@@ -10,8 +10,8 @@
 void entity_list_expand_draw(ComponentDraw **l, size_t list_size_original, size_t list_size_final);
 
 
-void entity_add_component_position(ComponentPosition *p, int list_size, unsigned int id, int x, int y);
-void entity_add_component_draw(ComponentDraw *d, int list_size, unsigned int id, int xoff, int yoff, int col, char sym);
-void entity_add_component_stats(ComponentStats *s, int list_size, unsigned int id, int hp, int str, int per, int agi);
+void entity_add_component_position(ComponentPosition *c, int list_size, unsigned int id, int x, int y);
+void entity_add_component_draw(ComponentDraw *c, int list_size, unsigned int id, int xoffset, int yoffset, int color, char symbol);
+void entity_add_component_stats(ComponentStats *c, int list_size, unsigned int id, int hp, int str, int per, int agi);
 
 #endif /*ENTITY_H*/
--- src/entity.c
+++ src/entity.c
@@ -6,30 +6,42 @@
 #include "../include/entity.h"
 
 
-void entity_add_component_position(ComponentPosition p[],int list_size, unsigned int id, int x, int y){
+void entity_add_component_position(ComponentPosition c[],int list_size, unsigned int id, int x, int y){
 
 	for(int i = 0; i < list_size; ++i){
-		if (p[i].id == 0){
-			p[i].id = id;	
-			p[i].x = x;
-			p[i].y = y;
+		if (c[i].id == 0){
+			c[i].id = id;	
+			c[i].x = x;
+			c[i].y = y;
 			break;
 		}
 	}
 }
 
-void entity_add_component_draw(ComponentDraw d[], int list_size, unsigned int id, int xoff, int yoff, int col, char sym){
-	d->id = id;
-	d->xoffset = xoff;
-	d->yoffset = yoff;
-	d->color = col;
-	d->symbol = sym;
+void entity_add_component_draw(ComponentDraw c[], int list_size, unsigned int id, int xoffset, int yoffset, int color, char symbol){
+
+	for(int i = 0; i < list_size; ++i){
+		if (c[i].id == 0){
+			c[i].id = id;	
+			c[i].xoffset = xoffset;
+			c[i].yoffset = yoffset;
+			c[i].color = color;
+			c[i].symbol = symbol;
+			break;
+		}
+	}
 }
 
-void entity_add_component_stats(ComponentStats s[], int list_size, unsigned int id, int hp, int str, int per, int agi){
-	s->id = id;
-	s->hp = hp;
-	s->strength = str;
-	s->perception = per;
-	s->agility = agi;
+void entity_add_component_stats(ComponentStats c[], int list_size, unsigned int id, int hp, int str, int per, int agi){
+
+	for(int i = 0; i < list_size; ++i){
+		if (c[i].id == 0){
+			c[i].id = id;	
+			c[i].hp = hp;
+			c[i].strength = str;
+			c[i].perception = per;
+			c[i].agility = agi;
+			break;
+		}
+	}
 }

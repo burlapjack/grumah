cebb648a2b5844189fcc89420d64b95cf5f1c6de
burlapjack <burlapjack@yahoo.com>
2021-08-02 23:24:12 -0500

    New menu system created

--- src/system.c
+++ src/system.c
@@ -32,8 +32,15 @@ void system_input(WINDOW *win){
 }
 
 
-void system_menu(){
-
+void system_menu(WINDOW *w, ComponentMenuOption *menu_list, size_t menu_list_length, ComponentPosition *position_list, size_t position_list_length, unsigned int *game_state){
+	unsigned int x, y;
+	unsigned int target_id;
+	for(int i = 0; i < menu_list_length; ++i){
+		
+		if( menu_list[i].game_state == *game_state){
+			target_id = menu_list[i].game_state;
+		}	
+	}
 }	
 
 

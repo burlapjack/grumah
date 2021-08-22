7aa8f02e109794396ba3afb0d0fc4e703dded73e
burlapjack <burlapjack@yahoo.com>
2021-08-22 17:46:51 -0500

    Added map.c

---
+++ include/map.h
@@ -0,0 +1,10 @@
+/* map.h by burlapjack 2021
+ * 
+ */
+
+#ifndef MAP_H
+#define MAP_H
+
+void map_generate_srp(char *map[], unsigned int map_width, unsigned int map_height);
+
+#endif /* MAP_H */
---
+++ src/map.c
@@ -0,0 +1,10 @@
+/* map_generate.c * by burlapjack 2021
+ * procedural map generation
+ */
+
+
+
+/*---------------------- Simple Room Placement Map Gen Algorythm ------------------------------*/
+void map_generate_srp(char *map[], unsigned int map_width, unsigned int map_height){
+	/* coming soon, hopefully */	
+}

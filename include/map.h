a868ec285ad654321a25f3ae9a62f8714c130b76
burlapjack <burlapjack@yahoo.com>
2021-08-30 23:07:12 -0500

    Reworked map gen code, still buggy

--- include/map.h
+++ include/map.h
@@ -7,5 +7,7 @@
 
 char* map_init(char *map, int map_width, int map_height);
 
+int map_xy(int x, int y, int map_width);
+
 void map_generate_srp(char *map, int map_width, int map_height, int room_size_max, int room_size_min, int num_rooms);
 #endif /* MAP_H */
--- src/map.c
+++ src/map.c
@@ -23,6 +23,13 @@ static int rand_int(int n){
 	}
 }
 
+/*--------------------- Convert 2d coords to 1d array position --------------------------------------------------------------*/
+int map_xy(int x, int y, int map_width){
+	int i;
+	i = y*map_width + x;
+	return i;
+}
+
 /*---------------------- Init a map that is all walls ------------------------------------------------------------------------*/
 char* map_init(char *map, int map_width, int map_height){
 	map = (char *)malloc((map_height * map_width) * sizeof (char));
@@ -36,44 +43,62 @@ char* map_init(char *map, int map_width, int map_height){
 /*---------------------- Simple-Room-Placement Map Gen Algorithm -------------------------------------------------------------*/
 
 void map_generate_srp(char *map, int map_width, int map_height, int room_size_max, int room_size_min, int num_rooms){
-	int x,y,x2,y2;
-	int room_topleft, room_topright,room_bottomleft,room_bottomright; 
-	int n = 0;
-	
-	//ComponentPosition position_list[num_rooms];
-	//ComponentSize room_list[num_rooms];
+	int x,y,h,w;
+	int i,j,k,cell;
+	int n = 3;
+	unsigned int collision;
+
+	ComponentPosition rm_pos[num_rooms];
+	ComponentSize rm_size[num_rooms];
 
 	/*------------ Place rooms -------------------------------*/
 	while(n < num_rooms ){
 		/*-------- Propose new randomized room coords --------*/
 		x = rand_int(map_width - 2);	
 		y = rand_int(map_height - 2);	
-		x2 = rand_int(room_size_max) + x + room_size_min;	
-		y2 = rand_int(room_size_max) + y + room_size_min;	
+		w = rand_int(room_size_max);	
+		h = rand_int(room_size_max);	
 
 		/*-------- Check coords are within bounds of map -----*/
 		/*-------- Correct coords as needed ------------------*/
-		x = ((x > 1) ? x : 2);
-		y = ((y > 1) ? y : 2);
-		x2 = (x2 < (map_width-2) ? x2 : map_width - 2);
-		y2 = (y2 < (map_height-2) ? y2 : map_height - 2);
+		x = (x > 1 ? x : 2);
+		y = (y > 1 ? y : 2);
+		w = (w < map_width-2-x ? w : map_width - 2);
+		h = (h < map_height-2-y ? h : map_height - 2);
 		
-		/*-------- Translate 2D coords into 1D coords---------*/
-		int i,j,k;
-		room_topleft = y*map_width + x;
-		room_topright = y*map_width + x2;
-		room_bottomleft = y2*map_width + x;
-		room_bottomright = y2*map_width + x2;
-
 		/*-------- Prevent collisions with other rooms -------*/
-		if(map[room_topleft] == '#' && map[room_topright] == '#' && map[room_bottomleft] == '#' && map[room_bottomright] =='#'){
-			for(i = y; i < y2; i++){
-				for(j = x; j < x2; j++){
-					map[(i*map_width)+j]='.';
+		if( n > 0){
+			collision = 0;
+			for(i = 0; i < n; i++){
+				if( x < rm_pos[i].x + rm_size[i].width && (x + w) > rm_pos[i].x && y < rm_pos[i].y + rm_size[i].height && (y + h) > rm_pos[i].y){
+					collision = 1;
+					break;	
 				}
 			}	
-			n++;
+			if(collision == 0){
+				n++;
+				rm_pos[n].x = x;
+				rm_pos[n].y = y;
+				rm_size[n].width = w;
+				rm_size[n].height = h;
+			}
 		}
-	}	
+		else{
+			n++;
+			rm_pos[n].x = x;
+			rm_pos[n].y = y;
+			rm_size[n].width = w;
+			rm_size[n].height = h;
+		}
+	}
+
+	for(i = 0; i < num_rooms; i++){
+		for(j = rm_pos[i].y; j < rm_size[i].height + rm_pos[i].y; j++){
+			for(k = rm_pos[i].x; k < rm_size[i].width + rm_pos[i].x; k++){
+				cell = map_xy(k,j,map_width);
+				map[cell] = '.';
+			}
+		}
+	}
 }
 

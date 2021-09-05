b4bd44911457558c9a851df41728d3179d6bf9e3
burlapjack <burlapjack@yahoo.com>
2021-09-05 00:28:33 -0500

    Removed outdated component funcs

--- include/component.h
+++ include/component.h
@@ -79,19 +79,9 @@ ComponentTrigger* component_init_trigger(ComponentTrigger **c, unsigned int size
 Component* component_init(Component *c, unsigned int size_lists);
 
 /*----------- Deallocate Component List Memory ----------------*/
-void component_list_free_all(ComponentPosition *cp, ComponentDraw *cd, ComponentStats *cs, ComponentMenuOption *co);
-
 void component_free_all(Component *c);
 
-/*----------- Component List Size-Doubling Variant 1 ----------*/
-void component_list_size_double_draw(ComponentDraw** component_list, unsigned int *list_size);
-void component_list_size_double_menu_option(ComponentMenuOption** component_list, unsigned int *list_size);
-void component_list_size_double_position(ComponentPosition** component_list, unsigned int *list_size);
-void component_list_size_double_size(ComponentStats** component_list, unsigned int *list_size);
-void component_list_size_double_stats(ComponentStats** component_list, unsigned int *list_size);
-void component_list_size_double_trigger(ComponentTrigger** component_list, unsigned int *list_size);
-
-/*----------- Component List Size-Doubling Variant 2 ----------*/
+/*----------- Component List Size-Doubling --------------------*/
 void component_list_double_input(Component *c);
 void component_list_double_draw(Component *c);
 void component_list_double_menu_option(Component *c);
@@ -99,13 +89,6 @@ void component_list_double_position(Component *c);
 void component_list_double_size(Component *c);
 void component_list_double_stats(Component *c);
 void component_list_double_trigger(Component *c);
-
-/*----------- Component Additions Variant 1 -------------------*/
-void component_list_add_draw(ComponentDraw **component_list, unsigned int *list_size, unsigned int id, int color, char symbol);
-void component_list_add_menu_option(ComponentMenuOption **component_list, unsigned int *list_size, unsigned int id, char name[32], unsigned int game_state, unsigned int highlighted);
-void component_list_add_position(ComponentPosition **component_list, unsigned int *list_size, unsigned int id, int x, int y);
-void component_list_add_stats(ComponentStats **component_list, unsigned int *list_size, unsigned int id, unsigned int hp, unsigned int str, unsigned int per, unsigned int agi);
-void component_list_add_trigger(ComponentTrigger**component_list, unsigned int *list_size, unsigned int id, unsigned int game_state);
 
 /*----------- Component Container Additions -------------------*/
 void component_add_input(Component *c, unsigned int id);
--- src/component.c
+++ src/component.c
@@ -167,15 +167,7 @@ void component_delete_all(Component *c){
 	component_delete_all_trigger(c);	
 }
 
-
 /*-------------- Free all Component Lists -----------------------------------------------------------*/
-
-void component_list_free_all(ComponentPosition *cp, ComponentDraw *cd, ComponentStats *cs, ComponentMenuOption *co){
-	free(cp);
-	free(cd);
-	free(cs);
-	free(co);
-}
 
 void component_free_all(Component *c){
 	free(c->draw);
@@ -186,84 +178,6 @@ void component_free_all(Component *c){
 }
 
 /*---------------Double the size of an Component list -----------------------------------------------*/
-
-void component_list_size_double_position(ComponentPosition** component_list, unsigned int *list_size){
-	//test the reallocation
-	ComponentPosition *temp = realloc(*component_list, sizeof **component_list * (*list_size * 2));
-	if (temp == NULL) {
-		perror("Failure to reallocate component_list_position");
-		exit(EXIT_FAILURE);
-	}	
-	*component_list = temp;	
-
-	for(unsigned int i = *list_size; i < (*list_size * 2 ); ++i ){
-		(*component_list)[i].id = 0;
-	}
-	*list_size = *list_size * 2;
-}
-
-void component_list_size_double_draw(ComponentDraw** component_list, unsigned int *list_size){
-	//test the reallocation
-	ComponentDraw* temp = realloc(*component_list, sizeof (**component_list) * (*list_size * 2));
-	if (temp == NULL) {
-		perror("Failure to reallocate component_list_draw");
-		exit(EXIT_FAILURE);
-	}	
-	*component_list = temp;	
-
-	for(unsigned int i = *list_size; i < (*list_size * 2 ); ++i ){
-		(*component_list)[i].id = 0;
-	}
-	*list_size = *list_size * 2;
-}
-
-void component_list_size_double_stats(ComponentStats** component_list, unsigned int *list_size){
-	//test the reallocation
-	ComponentStats*temp = realloc(*component_list, sizeof **component_list * (*list_size * 2));
-	if (temp == NULL) {
-		perror("Failure to reallocate component_list_stats");
-		exit(EXIT_FAILURE);
-	}	
-	*component_list = temp;	
-
-	for(unsigned int i = *list_size; i < (*list_size * 2 ); ++i ){
-		(*component_list)[i].id = 0;
-	}
-	*list_size = *list_size * 2;
-}
-
-void component_list_size_double_menu_option(ComponentMenuOption** component_list, unsigned int *list_size){
-	//test the reallocation
-	ComponentMenuOption *temp = realloc(*component_list, sizeof **component_list * (*list_size * 2));
-	if (temp == NULL) {
-		perror("Failure to reallocate component_menu_option");
-		exit(EXIT_FAILURE);
-	}	
-	*component_list = temp;	
-
-	for(unsigned int i = *list_size; i < (*list_size * 2 ); ++i ){
-		(*component_list)[i].id = 0;
-	}
-	*list_size = *list_size * 2;
-}
-
-
-void component_list_size_double_trigger(ComponentTrigger** component_list, unsigned int *list_size){
-	//test the reallocation
-	ComponentTrigger *temp = realloc(*component_list, sizeof **component_list * (*list_size * 2));
-	if (temp == NULL) {
-		perror("Failure to reallocate component_trigger");
-		exit(EXIT_FAILURE);
-	}	
-	*component_list = temp;	
-
-	for(unsigned int i = *list_size; i < (*list_size * 2 ); ++i ){
-		(*component_list)[i].id = 0;
-	}
-	*list_size = *list_size * 2;
-}
-
-//EXPERIMENTAL
 
 void component_list_double_input(Component *c){
 	//test the reallocation
@@ -374,81 +288,6 @@ void component_list_double_trigger(Component *c){
 		c->trigger[i].id = 0;
 	}
 	c->size_trigger = c->size_trigger * 2;
-}
-
-/*---------------Add Component to entity list-----------------------------------------------------*/
-
-
-void component_list_add_draw(ComponentDraw **component_list, unsigned int *list_size, unsigned int id, int color, char symbol){
-	for(unsigned int i = 0; i < *list_size; ++i){
-		if (i == *list_size-1 && (*component_list)[i].id != 0){
-			component_list_size_double_draw(component_list,list_size);
-		}
-		else if ((*component_list)[i].id == 0){
-			(*component_list)[i].id = id;	
-			(*component_list)[i].color = color;
-			(*component_list)[i].symbol = symbol;
-			break;
-		}
-	}
-}
-
-void component_list_add_position(ComponentPosition **component_list,unsigned int *list_size, unsigned int id, int x, int y){
-	for(unsigned int i = 0; i < *list_size; ++i){
-		if (i == *list_size-1 && (*component_list)[i].id != 0){
-			component_list_size_double_position(component_list,list_size);
-		}
-		else if ((*component_list)[i].id == 0){
-			(*component_list)[i].id = id;	
-			(*component_list)[i].x = x;
-			(*component_list)[i].y = y;
-			break;
-		}
-	}
-}
-
-void component_list_add_stats(ComponentStats **component_list, unsigned int *list_size, unsigned int id, unsigned int hp, unsigned int str, unsigned int per, unsigned int agi){
-	for(unsigned int i = 0; i < *list_size; ++i){
-		if (i == *list_size-1 && (*component_list)[i].id != 0){
-			component_list_size_double_stats(component_list,list_size);
-		}
-		else if ((*component_list)[i].id == 0){
-			(*component_list)[i].id = id;	
-			(*component_list)[i].hp = hp;
-			(*component_list)[i].strength = str;
-			(*component_list)[i].perception = per;
-			(*component_list)[i].agility = agi;
-			break;
-		}
-	}
-}
-
-void component_list_add_menu_option(ComponentMenuOption **component_list, unsigned int *list_size, unsigned int id, char name[32], unsigned int game_state, unsigned int highlighted){
-	for(unsigned int i = 0; i < *list_size; ++i){
-		if (i == *list_size-1 && (*component_list)[i].id != 0){
-			component_list_size_double_menu_option(component_list,list_size);
-		}
-		else if ((*component_list)[i].id == 0){
-			(*component_list)[i].id = id;
-			strcpy((*component_list)[i].name, name ); /* check if this is right! */
-			(*component_list)[i].parent_id = game_state;	
-			(*component_list)[i].highlighted = highlighted;	
-			break;
-		}
-	}
-}
-
-void component_list_add_trigger(ComponentTrigger**component_list, unsigned int *list_size, unsigned int id, unsigned int game_state){
-	for(unsigned int i = 0; i < *list_size; ++i){
-		if (i == *list_size-1 && (*component_list)[i].id != 0){
-			component_list_size_double_trigger(component_list,list_size);
-		}
-		else if ((*component_list)[i].id == 0){
-			(*component_list)[i].id = id;
-			(*component_list)[i].game_state = game_state;
-			break;
-		}
-	}
 }
 
 /*-------------- Add Component to entity list using a Component container struct -----------------*/

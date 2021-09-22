7e5822506e4d9e3b55814533400aae9640ebd332
burlapjack <burlapjack@yahoo.com>
2021-09-22 11:43:20 -0500

    Added PathGraph type.

--- include/map.h
+++ include/map.h
@@ -45,13 +45,22 @@ typedef struct{
 	int x,y,x2,y2;
 }Hall;
 
-struct pathnode {
+struct pathnode{
 	int id;
-	int f,g,h;
 	int number_of_neighbors;
-	struct PathNode **neighbors;
+	struct PathNode **neighbors; /* array of pointers to neighboring nodes */
+	int heuristic; /* distance from the node to goal */
 };
 typedef struct pathnode PathNode;
+
+typedef struct{
+	int node_count; /* Number of nodes on the graph */
+	int iterations; /* number of times the loop has run */
+	PathNode **unvisited_nodes; /* ordered array of PathNode pointers on the graph */
+	PathNode **visited_nodes; /* array of pointers to PathNodes marked as "visited".  Empty by default */
+	PathNode **parents; /* array of pointers to each node's parent node which creates  the shortest distance from origin */
+	int *distances; /* array that represents the total distance from an origin node to every other node */
+}PathGraph;
 
 /*--------Map Init / Free from memory ----------------------------------------------------------------------------------------*/
 void map_init(MapData *m, int map_width, int map_height);

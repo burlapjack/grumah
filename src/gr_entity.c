695baf8fc9215fac644295d90fa740455f587e27
burlapjack <burlapjack@yahoo.com>
2021-11-04 23:43:59 -0500

    added gr_entity_add_actor

--- src/gr_entity.c
+++ src/gr_entity.c
@@ -4,7 +4,16 @@
 
 #include <stdlib.h>
 #include "../include/gr_component.h"
+#include "../include/gr_map.h"
 #include "../include/gr_entity.h"
+
+int gr_entity_add_actor(Component *c, MapData * m, int x, int y, char symbol){
+	gr_component_add_position(c, x, y);
+	gr_component_add_draw(c, 1, 1, symbol);
+	gr_component_add_attributes(c, 0, 0, 0);
+	c->next_id++;
+	return c->next_id;
+}
 
 int gr_entity_add_player(Component *c, int x, int y){
 	gr_component_add_position(c, x, y);

de83355c7e8a6a2d8e4748221d6297923641d9d1
burlapjack <burlapjack@yahoo.com>
2021-11-09 14:30:55 -0600

    Input file changes. Experimental.

--- src/gr_input.c
+++ src/gr_input.c
@@ -21,26 +21,13 @@ void gr_input_init(InputData *i){
 };
 
 void gr_input_file_get_custom_values(InputData *i){
-	char tag[15];
+	char tag[20];
 	int value;
-	//if(access("../settings.txt", F_OK|R_OK) == 0){
-		FILE *fp;
-		fp = fopen("settings.txt", "r");
-		//while(fscanf(fp, "%s %d", tag, &value) != EOF){
-			fscanf(fp, "%s %d", tag, &value);
-			//if(strcmp(tag, "up") == 0) i->up = value;
-			//else if(strcmp(tag,"down") == 0) i->down = value;
-			//else if(strcmp(tag,"left") == 0) i->left = value;
-			//else if(strcmp(tag,"right") == 0) i->right = value;
-			//else if(strcmp(tag,"up_right") == 0) i->up_right = value;
-			//else if(strcmp(tag,"up_left") == 0) i->up_left = value;
-			//else if(strcmp(tag,"down_right") == 0) i->down_right = value;
-			//else if(strcmp(tag,"down_left") == 0) i->down_left = value;
-			//else if(strcmp(tag,"inventory") == 0) i->inventory = value;
-			//else if(strcmp(tag,"select") == 0) i->select = value;
-			//else if(strcmp(tag,"back") == 0) i->back = value;
-		//}
-			fclose(fp);
-	//}
+	FILE *fp;
+	if((fp = fopen("../settings.txt", "r")) != NULL){
+		fscanf(fp, "%c %d", tag, &value);
+		printw("%d", value);
+		fclose(fp);
+	}
 }
 
